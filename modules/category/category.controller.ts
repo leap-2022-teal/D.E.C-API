3fb3aeaa272c441c801e653fdcf70228732529a3
Nyambaa0303 <nyambaatar0303@gamil.com>
2023-04-26 16:26:03 +0800

    deleteMany

--- modules/category/category.controller.ts
+++ modules/category/category.controller.ts
@@ -30,6 +30,7 @@ export async function createNewCategory(req: Request, res: Response) {
 export async function deleteCategoryById(req: Request, res: Response) {
   const { id } = req.params;
   await category.findByIdAndRemove({ _id: id });
+  await category.deleteMany({ parentId: id });
   res.status(200);
 }
 export async function updateCategoryById(req: Request, res: Response) {
