ef1a2d1c0d24b8e870326f36b34ed36581fa54d6
TealMunkhtavilan <tsgaan@gmail.com>
2023-04-27 17:36:08 +0800

    nav

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import NavbarItems from "./NavbarItems";
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import Categories from "../menu";
 
 export default function NavBar() {
   return (
@@ -21,9 +22,7 @@ export default function NavBar() {
         <div className="px-20 md:px-16 py-2 flex flex-row justify-between items-center  bg-opacity-90">
           
             <div className="flex-row ml-8 gap-7 hidden lg:flex justify-center w-full">
-              <NavbarItems label="Nike"/>
-              <NavbarItems label="Adidas" />
-              <NavbarItems label="Puma" />
+              <Categories/>
             {/* <NavbarItems label={<Link href={"../auth/signin"}>Sign in</Link>} /> */}
             </div>
             <div className="flex-row ml-8 gap-4 hidden lg:flex">
