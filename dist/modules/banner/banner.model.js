9897dea1cc2d26de319c88c229135773d2535346
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2023-05-04 10:28:46 +0800

    api user modal deer oorchlolt

--- dist/modules/banner/banner.model.js
+++ dist/modules/banner/banner.model.js
@@ -3,14 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Banner = void 0;
 const mongoose_1 = require("mongoose");
 const BannerSchema = new mongoose_1.Schema({
-  name: { type: String },
-  image: {
-    path: { type: String, default: "" },
-    width: { type: Number, default: 0 },
-    height: { type: Number, default: 0 },
-  },
-  details: { type: String },
-  link: { type: String },
-  categoryId: { type: String, ref: "category.categoryId" },
+    name: { type: String },
+    image: {
+        path: { type: String, default: "" },
+        width: { type: Number, default: 0 },
+        height: { type: Number, default: 0 },
+    },
+    details: { type: String },
+    link: { type: String },
 });
 exports.Banner = (0, mongoose_1.model)("Banner", BannerSchema);
